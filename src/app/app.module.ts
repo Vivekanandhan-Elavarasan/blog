eecc8804263272e0363bd31034598e4de32d2c60
Vivekanandhan-Elavarasan <63054867+Vivekanandhan-Elavarasan@users.noreply.github.com>
2020-06-01 23:54:34 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    // FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
